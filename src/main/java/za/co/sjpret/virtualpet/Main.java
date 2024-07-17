d426f5f79b5ec2df084f69db0b96e37b0fecfe1b
SJ-Pretorius <sjpret12345@gmail.com>
2024-07-17 21:39:25 +0200

    Only one instance of the program can run now. (Using ServerSocket)

--- src/main/java/za/co/sjpret/virtualpet/Main.java
+++ src/main/java/za/co/sjpret/virtualpet/Main.java
@@ -2,8 +2,17 @@ package za.co.sjpret.virtualpet;
 
 import za.co.sjpret.virtualpet.controller.Controller;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+
 public class Main {
     public static void main(String[] args) {
-        new Controller();
+        try (ServerSocket socket = new ServerSocket(9999)) {
+            System.out.println("Application is running.");
+            new Controller();
+        } catch (IOException e) {
+            System.out.println("Another instance is running.");
+            System.exit(1);
+        }
     }
 }
